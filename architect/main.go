ebd5588412fe8dff62bc9cde161994d4ed30374d
uiop <uiop@wasdhjkl.xyz>
2025-09-03 18:12:58 -0400

    feat(architect): exit on failure

--- architect/main.go
+++ architect/main.go
@@ -9,11 +9,13 @@ func main() {
 	cfg, err := newConfig()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create config: %v", err)
+		os.Exit(1)
 	}
 
 	srv, err := newServer(cfg)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create server: %v", err)
+		os.Exit(1)
 	}
 
 	srv.run()
