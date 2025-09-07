079421684413dfbce775563da8888e86edd0c33f
uiop <uiop@wasdhjkl.xyz>
2025-09-07 15:07:04 -0400

    feat(architect): add graceful shutdown
    - Implement handling of TERM signals to stop server
    - Start the server in a goroutine and block until signal received
    - Use one second timeout for graceful shutdown

--- architect/server.go
+++ architect/server.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 type server struct {
+	http.Server
 	config *config
-	router *mux.Router
 }
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +24,34 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) run() {
-	http.ListenAndServe(s.config.addr, s.router)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "Server failed: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-c // Block until signal received
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "Server forced to shutdown: %v", err)
+	}
 }
 
 func newServer(cfg *config) (*server, error) {
 	r := mux.NewRouter()
 	r.HandleFunc("/", authHandler)
-	return &server{config: cfg, router: r}, nil
+
+	return &server{
+		Server: http.Server{
+			Addr:    cfg.addr,
+			Handler: r,
+		},
+		config: cfg,
+	}, nil
 }
