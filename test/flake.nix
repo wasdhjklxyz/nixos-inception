36185dfbbadd2a2c09824e35a80fe274fe5554c7
uiop <uiop@wasdhjkl.xyz>
2025-12-19 23:15:40 -0500

    refactor: test/foo.nix -> test/config.nix

--- test/flake.nix
+++ test/flake.nix
@@ -12,7 +12,7 @@
   outputs = { self, nixpkgs, nixos-inception }: {
     nixosConfigurations.foo = nixos-inception.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./foo.nix ];
+      modules = [ ./config.nix ];
       deployment = {
         ageKeyFile = "./secrets/key.txt";
         serverAddr = "10.0.2.2";
